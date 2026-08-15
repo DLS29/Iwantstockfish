a4c94fd1a14ba90f8bb9e0b08eb16a5e3a0b4797
DLS29 <danyagold10@gmail.com>
2026-08-15 11:33:28 +0300

    the line of sight fix and possible move draw are working mostly properly

--- Backend/Backend/board.py
+++ Backend/Backend/board.py
@@ -30,10 +30,22 @@ class board:
             self.y = y
         def piecemove(self,x:int,y:int) -> bool:
             if self.name == "P":
-                if y - self.y == 1 and self.x == x:return True
+                canmove = check_empty(board_main.piece_list,self.x,self.y + 1) and check_empty(board_main.piece_list,self.x,y)
+                if y - self.y == 2 and self.x == x and self.y == 1 and canmove:return True
+                if y - self.y == 1 and self.x == x and check_empty(board_main.piece_list,self.x,y):return True
+                if y - self.y == 1 and abs(self.x - x) == 1 and check_capture(board.piece_list,x,y,self.name):return True
                 return False
             if self.name == "p":
-                if self.y - y == 1 and self.x == x:return True
+                canmove = check_empty(board_main.piece_list,self.x,self.y - 1) and check_empty(board_main.piece_list,self.x,y)
+                if self.y - y  == 2 and self.x == x and self.y == 6 and canmove:return True
+                if self.y - y == 1 and self.x == x and check_empty(board_main.piece_list,self.x,y):return True
+                if self.y - y == 1 and abs(self.x - x) == 1 and check_capture(board.piece_list,x,y,self.name):return True
+                return False
+            if self.name == "K":
+                if abs(self.x - x) <= 1 and abs(self.y - y) <= 1 and (x,y) not in attacked_squares_b:return True
+                return False
+            if self.name == "k":
+                if abs(self.x - x) <= 1 and abs(self.y - y) <= 1 and (x,y) not in attacked_squares_w:return True
                 return False
             if self.name == "B" or self.name == "b":
                 if abs(self.x - x) == abs(self.y - y):return True
@@ -51,16 +63,18 @@ class board:
                 if self.x != x and self.y == y:return True
                 if abs(self.x - x) == abs(self.y - y):return True
                 return False
-            if self.name == "K" or self.name == "k":
-                if abs(self.x - x) == 1 and abs(self.y - y) == 1:return True
-                return False
             if self.name == "E" or self.name == "E":return True
             return True
         def trymove(self,x:int,y:int) -> bool:
-            if check_line_sight(board.piece_list,self.x,x,self.y,y,self.name) and self.piecemove(x,y):
-                check_capture(board.piece_list,x,y,self.name)
+            gnrte_attack_square(board_main.piece_list)
+            line_sight = check_line_sight(board.piece_list,self.x,x,self.y,y,self.name)
+            correct_move = self.piecemove(x,y)
+            allowed_capture = check_capture(board_main.piece_list,x,y,self.name)or check_empty(board_main.piece_list,x,y)
+            print(self.name,line_sight,correct_move,allowed_capture,(self.x,))
+            if line_sight and correct_move and allowed_capture:
                 self.x = x
                 self.y = y
+                capture_piece(board_main.piece_list,x,y,self.name)
                 return True
             return False
 
@@ -74,14 +88,14 @@ def gnrte_fen_string(board_string:str) -> str:
         elif i == "/":
             if x != 0:
                 fen = fen + str(x)
-            x = 0
+                x = 0
             fen = fen + i
         else:
             if x != 0:
                 fen = fen + str(x)
                 x = 0
             fen = fen + i
-        if x != 0: fen = fen + str(x)
+    if x != 0: fen = fen + str(x)
     fen = fen + " " + next_move + " " + castling + " " + enpasse + " " + cmove + " " + nmove
     return fen
 def gnrte_board_ascii(piece_list) -> None:
@@ -90,11 +104,33 @@ def gnrte_board_ascii(piece_list) -> None:
         x[i.y] = x[i.y][:i.x] + i.name + x[i.y][i.x+1:]
     x = x[0] + "\n" + x[1] + "\n" +  x[2] + "\n" +  x[3] + "\n" + x[4] + "\n" + x[5] + "\n" +  x[6] + "\n" +  x[7]
     print(x)
-
+def gnrte_attack_square(piece_list) -> None:
+    attacked_squares_w.clear()
+    attacked_squares_b.clear()
+    for i in piece_list:
+        pieceid = i.name+str(i.x)+str(i.y)
+        possible_moves[pieceid] = []
+        if i.name == "P":
+            attacked_squares_w.add((i.x-1,i.y+1))
+            attacked_squares_w.add((i.x+1,i.y+1))
+        if i.name == "p":
+            attacked_squares_b.add((i.x-1,i.y-1))
+            attacked_squares_b.add((i.x+1,i.y-1))
+        for y in range(8):
+            for x in range(8):
+                line_sight = check_line_sight(board.piece_list,i.x,x,i.y,y,i.name)
+                correct_move = i.piecemove(x,y)
+                allowed_capture = check_capture(board_main.piece_list,x,y,i.name)or check_empty(board_main.piece_list,x,y)
+                if line_sight and correct_move:
+                    if i.name != "P" and i.name.islower() == False:attacked_squares_w.add((x,y))
+                    elif i.name != "p" and i.name.islower() == True:attacked_squares_b.add((x,y))
+                    if allowed_capture:
+                        possible_moves[pieceid].append((x,y))
 def update_fen_string(piece_list:list):
     x = ["00000000","00000000","00000000","00000000","00000000","00000000","00000000","00000000"]
     for i in piece_list:
-        x[i.y] = x[i.y][:i.x] + i.name + x[i.y][i.x+1:]
+        y = 7-i.y
+        x[y] = x[y][:i.x] + i.name + x[y][i.x+1:]
     x = x[0] + "/" + x[1] + "/" +  x[2] + "/" +  x[3] + "/" + x[4] + "/" + x[5] + "/" +  x[6] + "/" +  x[7]
     x = gnrte_fen_string(x)
     return x
@@ -112,103 +148,88 @@ def check_place(piece_list:list,x:int|list,y:int|list) -> bool:
             return True
         else:
             raise TypeError
+
+def diagonal_moves(piece_list:list,current_x:int,x:int,current_y:int,y:int) -> bool:
+    if abs(current_x - x) == 1:return True
+    x_list = []
+    y_list = []
+    x_dir = normalise(current_x-x)
+    y_dir = normalise(current_y-y)
+    for i in range(1,abs(current_x-x)):
+        x_list.append(current_x+x_dir*i)
+        y_list.append(current_y+y_dir*i)
+    return check_place(piece_list,x_list,y_list)
+def sides_moves(piece_list:list,current_x:int,x:int,current_y:int,y:int) -> bool:
+    if abs(current_x-x + current_y-y) == 1:return True
+    x_dir = normalise(current_x-x)
+    y_dir = normalise(current_y-y)
+    x_list = []
+    y_list = []
+    if x_dir != 0:
+        for i in range(1,abs(current_x-x)):
+            x_list.append(current_x + x_dir*i)
+            y_list.append(current_y)
+    else:
+        for i in range(1,abs(current_y-y)):
+            x_list.append(current_x)
+            y_list.append(current_y + y_dir*i)
+    return check_place(piece_list,x_list,y_list)
 def check_line_sight(piece_list:list,current_x:int,x:int,current_y:int,y:int,name:chr) -> bool:
-            name = name.capitalize()
-            if name == "P":
-                if check_place(piece_list,x,y):
-                    return True
-                return False
-            if name == "B":
-                if abs(current_x - x) == 1:
-                    if check_place(piece_list,x,y):
-                        return True
-                    return False
-                else:
-                    x_list = []
-                    y_list = []
-                    x_dir = normalise(current_x-x)
-                    y_dir = normalise(current_y-y)
-                    for i in range(1,abs(current_x-x)):
-                        x_list.append(current_x+x_dir*i)
-                        y_list.append(current_y+y_dir*i)
-                    if check_place(piece_list,x_list,y_list):
-                        return True
-                    return False
-            if name == "N":
-                return True
-            if name == "R":
-                x_dir = normalise(current_x-x)
-                y_dir = normalise(current_y-y)
-                if abs(current_x-x) == 1 or abs(current_y-y) == 1:
-                    return True
-                else:
-                    x_list = []
-                    y_list = []
-                    if x_dir != 0:
-                        for i in range(1,abs(current_x-x)):
-                            x_list.append(current_x + x_dir)
-                            y_list.append(y)
-                    else:
-                        for i in range(1,abs(current_y-y)):
-                            x_list.append(x)
-                            y_list.append(current_y + y_dir)
-                    if check_place(piece_list,x_list,y_list):
-                        return True
-                    return False
-            if name == "Q":
-                if abs(current_x - x) == 1:
-                    if check_place(piece_list,x,y):
-                        return True
-                    return False
-                else:
-                    x_list = []
-                    y_list = []
-                    x_dir = normalise(current_x-x)
-                    y_dir = normalise(current_y-y)
-                    for i in range(1,abs(current_x-x)):
-                        x_list.append(current_x+x_dir*i)
-                        y_list.append(current_y+y_dir*i)
-                    if check_place(piece_list,x_list,y_list):
-                        return True
-                    x_dir = normalise(current_x-x)
-                    y_dir = normalise(current_y-y)
-                    if x_dir == 1 or y_dir == 1:
-                        return True
-                    else:
-                        x_list = []
-                        y_list = []
-                        if x_dir != 0:
-                            for i in range(1,abs(current_x-x)):
-                                x_list.append(current_x + x_dir)
-                                y_list.append(y)
-                        else:
-                            for i in range(1,abs(current_y-y)):
-                                x_list.append(x)
-                                y_list.append(current_y + y_dir)
-                        if check_place(piece_list,x_list,y_list):
-                            return True
-                        return False
-            if name == "K":
-                return True
-            if name == "E":
-                return True
-            return True
-def check_capture(piece_list:list,x:int,y:int,name:chr) -> None:
+    name = name.capitalize()
+    if name == "P":
+        if current_x == x:
+            return check_place(piece_list,x,y)
+        return True
+    if name == "B":
+        return diagonal_moves(piece_list,current_x,x,current_y,y)
+    if name == "N":
+        return True
+    if name == "R":
+        return sides_moves(piece_list,current_x,x,current_y,y)
+    if name == "Q":
+        if abs(current_x-x) == abs(current_y-y):
+            return diagonal_moves(piece_list,current_x,x,current_y,y)
+        else:
+            return sides_moves(piece_list,current_x,x,current_y,y)
+    if name == "K":
+        return True
+    if name == "E":
+        return True
+    return True
+def check_capture(piece_list:list,x:int,y:int,name:chr) -> bool:
+    for i in piece_list:
+        if i.x == x and i.y == y and name.islower() != i.name.islower():return True
+        if i.x == x and i.y == y and name.islower() == i.name.islower():return False
+    return False
+def check_empty(piece_list,x:int,y:int) -> bool:
+    for i in piece_list:
+        if i.x == x and i.y == y:return False
+    return True
+def capture_piece(piece_list:list,x:int,y:int,name:chr) -> None:
     for i in piece_list:
         if i.x == x and i.y == y and name.islower() != i.name.islower():
             piece_list.remove(i)
+            return None
+    return None
 def normalise(n:int) -> int:
     if n==0: return 0
     if n < 0: return 1
     else: return -1
+def find_piece(x:int,y:int):
+    for i in board.piece_list:
+        if i.x == x and i.y == y:
+            return i
+    return None
 
-
+attacked_squares_b = set()
+attacked_squares_w = set()
+possible_moves = {}
 fen_string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 board_string = "rnbqkbnr/pppppppp/00000000/00000000/00000000/00000000/PPPPPPPP/RNBQKBNR"
 fen_board,next_move,castling,enpasse,cmove,nmove = fen_string.split(" ",7)
+board_main = board(fen_string)
 
 if __name__ == "__main__":
-    board_main = board(fen_string)
     print(*board_main.piece_list)
-    print(gnrte_fen_string(board_string))
     gnrte_board_ascii(board_main.piece_list)
+    gnrte_attack_square(board_main.piece_list)
--- frontend_main.py
+++ frontend_main.py
@@ -43,9 +43,14 @@ canvas.create_window(950,100,window=label, anchor="nw")
 canvas.create_window(950,200,window=button, anchor="nw")
 
 image_references = []
+pieceid = []
 def draw_board(fen_board:str):
+    for piece in pieceid:
+        canvas.delete(piece)
+    pieceid.clear()
+    image_references.clear()
     x = 0
-    y = 7
+    y = 0
     piece_dir = {
         "P":constance.piece_image_dir[7],
         "p":constance.piece_image_dir[6],
@@ -53,8 +58,8 @@ def draw_board(fen_board:str):
         "b":constance.piece_image_dir[0],
         "N":constance.piece_image_dir[5],
         "n":constance.piece_image_dir[4],
-        "K":constance.piece_image_dir[5],
-        "k":constance.piece_image_dir[4],
+        "K":constance.piece_image_dir[3],
+        "k":constance.piece_image_dir[2],
         "Q":constance.piece_image_dir[9],
         "q":constance.piece_image_dir[8],
         "R":constance.piece_image_dir[11],
@@ -62,14 +67,60 @@ def draw_board(fen_board:str):
     }
     for i in fen_board:
         if i == "/":
-            y = y - 1
+            y = y + 1
             x = 0
         elif i.isdigit():
             x = x +int(i)
         else:
             image = image_procesing(piece_dir[i])
             image_references.append(image)
-            canvas.create_image((x*SIZE) + (SIZE//2),(y*SIZE) + (SIZE//2),image=image,anchor="center")
+            pieceid.append(canvas.create_image((x*SIZE) + (SIZE//2),(y*SIZE) + (SIZE//2),image=image,anchor="center"))
             x = x + 1
+
+def in_area(xy1:tuple,xy2:tuple) -> bool:
+    return xy2[0]<=xy1[0]<=xy2[0]+xy2[2] and xy2[1]<=xy1[1]<=xy2[1]+xy2[3]
+
+possible_moves_selected = []
+def gnrte_posible_moves(piece):
+    for i in possible_moves_selected:
+        canvas.delete(i)
+    possible_moves_selected.clear()
+    board.gnrte_attack_square(board.board_main.piece_list)
+    piece_id_pm = piece.name+str(piece.x)+str(piece.y)
+    print(board.possible_moves[piece_id_pm])
+    moves = board.possible_moves.get(piece_id_pm, [])
+    if moves:
+        for i in board.possible_moves[piece_id_pm]:
+            x = i[0]
+            y = 7 -  i[1]
+            possible_moves_selected.append(canvas.create_rectangle(x*SIZE,y*SIZE,x*SIZE+SIZE,y*SIZE+SIZE,outline="brown",width=2))
+
+selected_square = None
+is_piece_selected = False
+selected_piece = None
+def select_piece(event) -> None:
+    global selected_square,is_piece_selected,selected_piece
+    if in_area((event.x,event.y),(0,0,SIZE*8,SIZE*8)):
+        if is_piece_selected:
+            col = event.x // SIZE
+            row = event.y // SIZE
+            selected_piece.trymove(col,7-row)
+            fen_board = board.update_fen_string(board.board.piece_list)
+            fen_board,_ = fen_board.split(" ",1)
+            draw_board(fen_board)
+            is_piece_selected = False
+            return None
+        canvas.delete(selected_square)
+        if in_area((event.x,event.y),(0,0,SIZE*8,SIZE*8)):
+            col = event.x // SIZE
+            row = event.y // SIZE
+            selected_square = canvas.create_rectangle(col*SIZE,row*SIZE,col*SIZE+SIZE,row*SIZE+SIZE,outline="yellow",width=5)
+            selected_piece = board.find_piece(col,7-row)
+            if selected_piece is not None:
+                is_piece_selected = True
+                gnrte_posible_moves(selected_piece)
+                return None
+canvas.bind("<Button-1>",select_piece)
+
 draw_board(board.fen_board)
 root.mainloop()
